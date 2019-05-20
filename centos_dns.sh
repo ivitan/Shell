72df3736748dc034b5ced2e7bf8d5eb91be1d039
Vitan <vitan.me@gmail.com>
2019-05-20 22:55:43 +0800

    📦 NEW: v0.0.2

--- centos_dns.sh
+++ centos_dns.sh
@@ -1,18 +1,64 @@
 #! /bin/bash
 # author Vitan
 # DNS Configer
+
+color1="\e[0;31;40m"
+color2="\e[0;32;40m"
+color3="\e[0;33;40m"
+color4="\e[0;34;40m"
+color5="\e[0;35;40m"
+color6="\e[0;37;40m"
+
+function logo()
+{
+echo -ne "\033[0;33m"
+cat<<EOT
+                                  _oo0oo_
+                                 088888880
+                                 88" . "88
+                                 (| -_- |)
+                                  0\ = /0
+                               ___/'---'\___
+                             .' \\\\|     |// '.
+                            / \\\\|||  :  |||// \\
+                           /_ ||||| -:- |||||- \\
+                          |   | \\\\\\  -  /// |   |
+                          | \_|  ''\---/''  |_/ |
+                          \  .-\__  '-'  __/-.  /
+                        ___'. .'  /--.--\  '. .'___
+                     ."" '<  '.___\_<|>_/___.' >'  "".
+                    | | : '-  \'.;'\ _ /';.'/ - ' : | |
+                    \  \ '_.   \_ __\ /__ _/   .-' /  /
+                ====='-.____'.___ \_____/___.-'____.-'=====
+                                  '=---='
+              ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+                        佛祖保佑    iii    永无 Bug
+EOT
+echo -ne "\033[m"
+}
+
+function line()
+{
+        echo -e "$color5--------------------------------------------------------"
+}
+
+logo
+line
+
 if [ `yum list installed | grep bind` -ne 0 ];then
 yum remove bind bind-chroot || rm -rf /var/named
 yum -y install bind bind-utils bind-chroot &> /dev/null
 echo "Bind is installed"
 else
 yum -y install bind bind-utils bind-chroot &> /dev/null
-echo -e "\033[32m  Installed!!!\033[0m\n"
+echo -e "$color2 Installed!!!"
 service named start
-echo -e "\033[32m Install Success!!!\033[0m\n"
+echo -e "$color3 Install Success!!!"
 fi
 
-echo "请输入主配置文件的名字(named)"
+line
+
+echo -e "$color3请输入主配置文件的名字(named)"
 read NAMED
 if [ $NAMED ];then
 cat << EOF > /var/named/chroot/etc/named.conf
@@ -54,13 +100,15 @@ include "/etc/named.root.key";
 EOF
 fi
 
-echo "请输入正向解析域配置文件名称(vitan.me)"
+line
+
+echo -e "$color3请输入正向解析域配置文件名称(vitan.me)"
 read ZHENGXIANG
 
-echo "请输入反向向解析域配置文件名称(149.28.197)"
+echo -e "$color3 请输入反向向解析域配置文件名称(149.28.197)"
 read FANXIANG
 
-echo -e "\033[32m 请输入反向IP(197.28.149) \033[0m\n"
+echo -e "$color3请输入反向IP(197.28.149) \033[0m\n"
 read FANIP
 
 if [ $ZHENGXIANG  ];then
@@ -80,6 +128,8 @@ zone "$FANIP.in-addr.arpa" IN {
 };
 EOF
 fi
+
+line
 
 cd /var/named/chroot/var/named
 cp -p named.localhost $ZHENGXIANG.zone
@@ -101,6 +151,8 @@ mail    IN      A               $FANXIANG.1
 www1    IN      CNAME           www.$ZHENGXIANG.
 EOF
 fi
+
+line
 
 cd /var/named/chroot/var/named
 cp -p named.loopback $FANXIANG.zone
@@ -124,14 +176,18 @@ www1    IN      CNAME           www.$ZHENGXIANG.
 EOF
 fi
 
-echo -e "\033[32m Restart named service \022[om\n"
-service named restart
+line
 
-echo "Cofiger resolv.conf"
+echo -e "$color3Restart named service"
+service named restart
+line
+echo -e "$color3Cofiger resolv.conf"
 cat << EOF > /etc/resolv.conf
 search $ZHENGXIANG
 nameserver $FANXIANG
 EOF
 
+line
+echo -e "$color3Finsh"
 
-echo -e "\033[32m Finsh \033[0m\n"
+line
