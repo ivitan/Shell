0e4ed38e6fd124e64863d4e8f53e59ab0f19bf43
Vitan <vitan.me@gmail.com>
2019-05-29 15:33:38 +0800

    🐛 FIX: a bug.

--- local_repo.sh
+++ local_repo.sh
@@ -6,7 +6,7 @@ color3="\e[0;33;40m"
 mkdir /local_repo
 mount /dev/cdrom /loca_repo
 
-PATH="etc/yum.repos.d/local_repo"
+PATH="etc/yum.repos.d/local_repo.repo"
 
 cat << EOF > $PATH
 [local_repo]
@@ -16,4 +16,4 @@ gpgcheck=0 # 关闭 Key 检查
 enabled=1 #开启本地源
 EOF
 
-echo "$color3Successful!"
+echo "$colori3 Successful!"
