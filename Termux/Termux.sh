6239824cd2ed262480fd860269adc9badaff867d
Vitan <vitan.me@gmail.com>
2020-01-02 16:31:00 +0800

    Fix zsh&vim

--- Termux/Termux.sh
+++ Termux/Termux.sh
@@ -67,14 +67,13 @@ function Termux(){
         ;;
         
         4)
-            apt install git zsh curl -y
-            sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+            apt install git zsh curl wet -y
+            sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
             chsh -s zsh
             git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
             ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
-            git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-            git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-completions
-            git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+            wget https://github.com/ivitan/UnixConfig/releases/download/zshrc/zshrc -O ~/.zshrc 
+            source ~/.zshrc
             echo -e "$blue Successfuly"
             line
             menu
@@ -99,7 +98,7 @@ function Termux(){
         6)
             apt install vim wget -y
             curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-            wget -O ~/.vimrc https://github.com/ivitan/UnixConfig/releases/download/vimrc/vimrc
+            wget https://github.com/ivitan/UnixConfig/releases/download/vimrc/vimrc  -O ~/.vimrc 
             echo -e "$blue Sucessfully"
             line
             menu
@@ -303,7 +302,7 @@ function  items() {
             if ! [ -x "$(command -v wget)" ];then
                 apt install wget -y
             fi
-            wget -O ~/yun.zip https://github.com/iikira/BaiduPCS-Go/releases/latest/download/v3.6.1/BaiduPCS-Go-v3.6.1-linux-arm64.zip
+            wget https://github.com/iikira/BaiduPCS-Go/releases/latest/download/v3.6.1/BaiduPCS-Go-v3.6.1-linux-arm64.zip -O ~/yun.zip
             if ! [ -x "$(command -v unzip)" ];then
                 apt install unzip -y
             fi
