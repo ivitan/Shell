36bf02104c595e0a223f339b508157d22bdf6779
Vitan <vitan.me@gmail.com>
2020-02-28 15:41:30 +0800

    📦 NEW: Script

---
+++ MyShell/GetPath/GetPath.sh
@@ -0,0 +1,10 @@
+
+#!/bin/bash
+#获取自身绝对路径
+echo 1：$(cd `dirname $0`; pwd)
+echo 2：$(dirname $(readlink -f $0))
+echo 3：$(cd $(dirname ${BASH_SOURCE:-$0});pwd)
+echo 4：$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+echo "5：${0##*/}"
+echo "6：$(cd $(dirname ${BASH_SOURCE:-$0});pwd)"
+echo "7：Error:$(dirname $(readlink -f ${BASH_SOURCE[0]}))"/"${0##*/}"
